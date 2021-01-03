7ef4d74d98e0111d863814100908bfcfbd8fb439
elizabethuglova <elizabethuglova@yandex.ru>
2021-01-03 16:40:23 +0300

    добавление фона и скатерти

--- catch_thefood.py
+++ catch_thefood.py
@@ -1,9 +1,23 @@
 import pygame
+import os
+import sys
 
 if __name__ == '__main__':
     pygame.init()
     size = width, height = 300, 500
     screen = pygame.display.set_mode(size)
+    screen.fill((135, 206, 250))
+
+    def load_image(name, colorkey=None):
+        fullname = os.path.join('data', name)
+        if not os.path.isfile(fullname):
+            print(f"Файл с изображением '{fullname}' не найден")
+            sys.exit()
+        image = pygame.image.load(fullname)
+        screen.blit(image, (0, 400))
+
+    load_image('skatert.jpg')
+
     pygame.display.flip()
     while pygame.event.wait().type != pygame.QUIT:
         pass
