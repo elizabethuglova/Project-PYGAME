d08304026aac9c15029deacc1a4b96961e5330df
elizabethuglova <elizabethuglova@yandex.ru>
2021-01-09 20:22:37 +0300

    подсчёт очков

--- catch_thefood.py
+++ catch_thefood.py
@@ -3,7 +3,6 @@ from random import randint
 import os
 import sys
 
-
 pygame.init()
 pygame.time.set_timer(pygame.USEREVENT, 3000)
 
@@ -35,25 +34,35 @@ if not os.path.isfile(fullname2):
     sys.exit()
 image2 = pygame.image.load(fullname2)
 life_fon = pygame.transform.scale(image2, (50, 50))
-font = pygame.font.SysFont('arial', 25)
+life_font = pygame.font.SysFont('arial', 25)
 
-#  загрузка корзины
-fullname3 = os.path.join('data', 'korzina.png')
+#  загрузка фона для подсчета очков
+fullname3 = os.path.join('data', 'score_fon.png')
 if not os.path.isfile(fullname3):
     print(f"Файл с изображением '{fullname3}' не найден")
     sys.exit()
-image = pygame.image.load(fullname3)
+image2 = pygame.image.load(fullname3)
+score_fon = pygame.transform.scale(image2, (90, 40))
+score_font = pygame.font.SysFont('arial', 25)
+
+#  загрузка корзины
+fullname4 = os.path.join('data', 'korzina.png')
+if not os.path.isfile(fullname4):
+    print(f"Файл с изображением '{fullname4}' не найден")
+    sys.exit()
+image = pygame.image.load(fullname4)
 korzina = pygame.transform.scale(image, (100, 50))
 korzina_rect = korzina.get_rect(centerx=w // 2, bottom=h - 95)
 
 
 #  спрайты еда
 class Food(pygame.sprite.Sprite):
-    def __init__(self, x, speed, surf, life, group):
+    def __init__(self, x, speed, surf, life, score, group):
         pygame.sprite.Sprite.__init__(self)
         self.image = surf
         self.rect = self.image.get_rect(center=(x, 0))
         self.life = life
+        self.score = score
         self.speed = speed
         self.add(group)
 
@@ -68,12 +77,16 @@ food_images = ['data/apple.png', 'data/avocado.png', 'data/burger.png', 'data/co
                'data/pizza.png', 'data/sushi.png',
                'data/boot.png', 'data/disk.png', 'data/phone.png', 'data/toy.png']
 
-food_t = {'data/apple.png': 0, 'data/avocado.png': 0, 'data/burger.png': 0,
-          'data/cookies.png': 0, 'data/orange.png': 0,
-          'data/pizza.png': 0, 'data/sushi.png': 0,
-          'data/boot.png': 1, 'data/disk.png': 1, 'data/phone.png': 1, 'data/toy.png': 1}
+food_life = {'data/apple.png': 0, 'data/avocado.png': 0, 'data/burger.png': 0,
+             'data/cookies.png': 0, 'data/orange.png': 0,
+             'data/pizza.png': 0, 'data/sushi.png': 0,
+             'data/boot.png': 1, 'data/disk.png': 1, 'data/phone.png': 1, 'data/toy.png': 1}
 
-food_not_eat = ['data/boot.png', 'data/disk.png', 'data/phone.png', 'data/toy.png']
+food_score = {'data/apple.png': 50, 'data/avocado.png': 50, 'data/burger.png': 50,
+              'data/cookies.png': 100, 'data/orange.png': 100,
+              'data/pizza.png': 200, 'data/sushi.png': 200,
+              'data/boot.png': 0, 'data/disk.png': 0, 'data/phone.png': 0, 'data/toy.png': 0}
+
 
 food_surf1 = [pygame.image.load(i).convert_alpha() for i in food_images]
 
@@ -87,19 +100,23 @@ def createFood(group):
     index = randint(0, len(food_surf) - 1)
     x = randint(20, w - 20)
     speed_food += 0.08
-    return Food(x, speed_food, food_surf[index], food_t[food_images[index]], group)
+    return Food(x, speed_food, food_surf[index], food_life[food_images[index]],
+                food_score[food_images[index]], group)
 
 
 createFood(food)
 
 life = 3
+score = 0
 
 
 def catchFood():
     global life
+    global score
     for i in food:
         if korzina_rect.collidepoint(i.rect.center):
             life -= i.life
+            score += i.score
             if life == 0:
                 sys.exit()
             i.kill()
@@ -127,8 +144,12 @@ while running:
     screen.fill((135, 206, 250))
 
     screen.blit(life_fon, (0, 0))
-    text = font.render(str(life), 1, (0, 0, 0))
-    screen.blit(text, (19, 11))
+    text1 = life_font.render(str(life), 1, (0, 0, 0))
+    screen.blit(text1, (19, 11))
+
+    screen.blit(score_fon, (210, 5))
+    text2 = score_font.render(str(score), 1, (0, 0, 0))
+    screen.blit(text2, (235, 11))
 
     screen.blit(skatert, (0, 400))
     food.draw(screen)
